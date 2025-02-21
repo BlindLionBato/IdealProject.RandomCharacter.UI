a60494238744528b0a93cb1fcaa5713498f47838
BlindLionBato <blindlionbato@gmail.com>
2025-02-21 19:42:39 +0100

    feat: add Docker, ECR access, and reverse proxy setup
    Implemented Docker installation and containerization for the app. Replaced S3-related configurations with ECR access policies and roles for EC2. Added HTTPS reverse proxy setup with Apache for secure app routing.

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4"
-    }
-  }
-
   backend "s3" {
     bucket         = "ideal-project-terraform-states"
     key            = "dev/random-character-ui.tfstate"
@@ -26,35 +19,4 @@ resource "aws_instance" "random_character_server" {
   vpc_security_group_ids = [aws_security_group.ec2_instance_security_group.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   user_data            = file("./terraform-data/ec2/bootstrap.sh")
-}
-
-resource "aws_s3_bucket" "artefacts_s3_bucket" {
-  bucket = "ideal-project-random-character-ui-artefacts"
-}
-
-resource "aws_s3_bucket_ownership_controls" "artefacts_s3_bucket_ownership" {
-  bucket = aws_s3_bucket.artefacts_s3_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "artefacts_s3_bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.artefacts_s3_bucket_ownership]
-
-  bucket = aws_s3_bucket.artefacts_s3_bucket.id
-  acl    = "private"
-}
-
-resource "archive_file" "random_character_ui_artefact" {
-  type        = "zip"
-  source_dir = "./dist"
-  output_path = "./dist.zip"
-}
-
-resource "aws_s3_object" "random_character_ui_artefact_s3_object" {
-  bucket = aws_s3_bucket.artefacts_s3_bucket.bucket
-  key    = "dist.zip"
-  source = archive_file.random_character_ui_artefact.output_path
 }
