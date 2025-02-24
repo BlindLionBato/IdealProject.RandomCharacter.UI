ca0f4828b668014e4df806c25af913263a1d51d3
BlindLionBato <blindlionbato@gmail.com>
2025-02-24 19:19:30 +0100

    refactor: modularize IAM and security group resources
    Reorganized IAM policies, roles, and security groups into distinct modules for better modularity and reusability. Simplified resource management by integrating outputs and reducing hardcoded values. Adjusted file paths and updated references to align with the new structure.

--- terraform.tf
+++ terraform.tf
@@ -12,11 +12,26 @@ provider "aws" {
 
 }
 
+module "roles" {
+  source = "./infrastructure/roles"
+}
+
+module "security_groups" {
+  source = "./infrastructure/security-groups"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2_Instance_Profile"
+  role = module.roles.ec2_access_role_name
+}
+
 resource "aws_instance" "random_character_server" {
   ami                  = "ami-0343a21cd4b9d8ee8"
   instance_type        = "t2.micro"
   key_name             = "IdealProject.EC2.KeyPair"
-  vpc_security_group_ids = [aws_security_group.ec2_instance_security_group.id]
+  vpc_security_group_ids = [
+    module.security_groups.allow_default_connections_id
+  ]
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-  user_data            = file("./terraform-data/ec2/bootstrap.sh")
+  user_data            = file("./infrastructure/scripts/ec2/bootstrap.sh")
 }
