a82f89fda2fbc6f7c8b54d46d0cdd6da9ad17f47
Aleksey Goretskiy <blindlionbato@gmail.com>
2025-05-09 18:52:45 +0200

    feat: migrate deployment from EC2 to EKS via Terraform
    Replaced EC2-based infrastructure with an EKS cluster setup. Updated Terraform configurations for VPC, subnets, route tables, and IAM roles to support EKS. Added Kubernetes manifests for deployment and service management.

--- data.tf
+++ data.tf
@@ -1,24 +1,23 @@
 # Move to Terraform.Infrastructure.IdealProject.Global
 
 data "aws_route53_zone" "main" {
-  name = "random-character.org"  # Укажите ваш домен
+  name = local.domain_name
 }
 
-data "aws_acm_certificate" "random-character" {
-  domain             = "random-character.org"
-  tags = {
-    ProjectName = "IdealProject.RandomCharacter"
-  }
-}
+data "aws_ami" "eks_node_ami" {
+  owners      = ["602401143452"]
+  most_recent = true
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
   filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]  # Получаем все подсети для default VPC
+    name   = "name"
+    values = ["amazon-eks-node-${local.kubernetes_version}-v*"]
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
