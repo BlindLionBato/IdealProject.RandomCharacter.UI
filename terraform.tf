1e6abf3090f45e24061933e44c156f2ad3fa53b0
BlindLionBato <BlindLionBato@gmail.com>
2025-06-06 08:29:38 +0200

    feat: move EKS cluster-related resources and policies to a separate project
    Removed all resources and policies related to the EKS cluster, including its Terraform modules, IAM roles, policies, and configurations. Simplified CI/CD configuration by updating the AWS role and removing cluster references. Cleared out unnecessary files to streamline the infrastructure.

--- terraform.tf
+++ terraform.tf
@@ -11,17 +11,3 @@ terraform {
 provider "aws" {
 
 }
-
-module "statements" {
-  source = "git@github.com:BlindLionBato/Terraform.AWS.Security.Statements.git"
-}
-
-module "global" {
-  source = "git@github.com:BlindLionBato/Terraform.Infrastructure.Global.git"
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
